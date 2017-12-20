00fef13f52ab0bf19ad896d05ce60f868d65ec08
ykadir42 <ykadir42@gmail.com>
2017-12-20 00:37:45 -0500

    fork this

--- forking_server.c
+++ forking_server.c
@@ -12,10 +12,43 @@ static void sighandler(int signo) {
 }
 
 int main() {
+
+	int from_client;
+
+	while(1){
+		from_client = server_setup();
+		if(from_client > 0){
+			int f = fork();
+			if (!f) subserver(from_client);
+		}
+	}
+
+	return 0;
 }
 
 void subserver(int from_client) {
+	char buffer[BUFFER_SIZE];
+	int to_client;
+	to_client = server_connect(from_client);
+	while(1){
+		read(from_client, buffer, BUFFER_SIZE);
+		printf("[server] received: %s\n", buffer);
+
+		if(!strcmp(buffer, "exit")){
+			PRINT("[server] client disconnected");
+			close(to_client);
+			close(from_client);
+			break;
+		}
+
+		process(buffer);
+		write(to_client, buffer, BUFFER_SIZE);
+		printf("[server] sent: %s\n", buffer);
+	}
 }
 
 void process(char * s) {
+	int i;
+	for(i = 0; i < strlen(s); i++)
+		s[i] = (s[i] + 14) % 94 + 33;
 }
